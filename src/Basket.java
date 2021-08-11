2b34ae09d9f10f827b7f05271778863cb3c73b48
Lex-Smith <e.d.smith@mail.ru>
2021-08-12 00:09:55 +0300

    totalPrice added

--- src/Basket.java
+++ src/Basket.java
@@ -1,21 +1,30 @@
 public class Basket { //класс
-    private static String items = "";//??? скорее всего переменная
+    private static String items = "";//переменная
+    private static int totalPrice = 0;
 
     public static void main(String[] args) { //метод
         add( "Колбаса", 76);
         add( "Молоко", 55);
         add("масло", 120);
         print("Содержимое корзмны:");
+        System.out.println(getTotalPrice());
         clear();
         print("Содержимое корзмны:");
+        System.out.println(getTotalPrice());
     }
 
     public static void add(String name, int price) { //метод добавления в корзину с 2-я параметрами (переменные)
         items = items + "\n" + name + " - " + price;
+        totalPrice = totalPrice + price;
     }
 
     public static void clear() {
         items = "";
+        totalPrice = 0;
+    }
+
+    public static int getTotalPrice () {
+        return totalPrice;
     }
 
     public static void print(String title) {
